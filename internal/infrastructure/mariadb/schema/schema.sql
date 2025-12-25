5a8b5f33b67b28eaa21a09af764dd231fb020bac
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2025-12-25 13:15:49 +0900

    chore: 🎨 sqlfluffを導入・フォーマット

--- internal/infrastructure/mariadb/schema/schema.sql
+++ internal/infrastructure/mariadb/schema/schema.sql
@@ -1,18 +1,20 @@
 -- Create "vm_templates" table
 CREATE TABLE `vm_templates` (
-	`vmid` int unsigned NOT NULL,
-	`name` varchar(100) NOT NULL,
-	PRIMARY KEY (`vmid`)
+    `vmid` int unsigned NOT NULL,
+    `name` varchar(100) NOT NULL,
+    PRIMARY KEY (`vmid`)
 ) COLLATE utf8mb4_uca1400_ai_ci;
 
 -- Create "instances" table
 CREATE TABLE `instances` (
-	`vmid` int unsigned NOT NULL,
-	`user_id` varchar(100) NOT NULL,
-	`template_vmid` int unsigned NOT NULL,
-	`ip_address` varchar(45) NULL,
-	`created_at` timestamp NULL DEFAULT (current_timestamp()),
-	PRIMARY KEY (`vmid`),
-	INDEX `instances_vm_templates_FK` (`template_vmid`),
-	CONSTRAINT `instances_vm_templates_FK` FOREIGN KEY (`template_vmid`) REFERENCES `vm_templates` (`vmid`) ON UPDATE CASCADE ON DELETE RESTRICT
+    `vmid` int unsigned NOT NULL,
+    `user_id` varchar(100) NOT NULL,
+    `template_vmid` int unsigned NOT NULL,
+    `ip_address` varchar(45) NULL,
+    `created_at` TIMESTAMP NULL DEFAULT (current_timestamp()),
+    PRIMARY KEY (`vmid`),
+    INDEX `instances_vm_templates_FK` (`template_vmid`),
+    CONSTRAINT `instances_vm_templates_FK`
+    FOREIGN KEY (`template_vmid`) REFERENCES `vm_templates` (`vmid`)
+    ON UPDATE CASCADE ON DELETE RESTRICT
 ) COLLATE utf8mb4_uca1400_ai_ci;
